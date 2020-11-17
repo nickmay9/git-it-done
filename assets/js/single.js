e4dc3c0ae93ccbbd417dbd724708477813b7a889
Nick May <nickmay9@outlook.com>
2020-11-17 12:30:43 -0500

    display issues along with error handling

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,4 +1,5 @@
 var issueContainerEl = document.querySelector("#issues-container");
+var limitWarningEl = document.querySelector("#limit-warning");
 
 function getRepoIssues(repo) {
     var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
@@ -7,6 +8,11 @@ function getRepoIssues(repo) {
         if(response.ok) {
             response.json().then(function(data){
                 displayIssues(data);
+
+                //check if api has paginated issues
+                if(response.headers.get("Link")){
+                    displayWarning(repo);
+                }
             });
         }
         else {
@@ -55,4 +61,16 @@ function displayIssues(issues){
     }
 }
 
-getRepoIssues("octocat/linguist");
+function displayWarning(repo) {
+    limitWarningEl.textContent = "To see more than 30 issues, visit ";
+
+    var linkEl = document.createElement("a");
+    linkEl.textContent = "See More Issues on GitHub.com";
+    linkEl.setAttribute("href", "https://github.com/" + repo + "/issues");
+    linkEl.setAttribute("target", "_blank");
+
+    //append to warning container
+    limitWarningEl.appendChild(linkEl);
+}
+
+getRepoIssues("octocat/Spoon-Knife");
