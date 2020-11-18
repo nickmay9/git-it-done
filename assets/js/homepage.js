b74d404330aff68143c08e7e704521c1b01ea98f
Nick May <nickmay9@outlook.com>
2020-11-18 10:01:22 -0500

    added buttons to for specific languages

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -2,6 +2,7 @@ var userFormEl = document.querySelector("#user-form");
 var nameInputEl = document.querySelector("#username");
 var repoContainerEl = document.querySelector("#repos-container");
 var repoSearchTerm = document.querySelector("#repo-search-term");
+var languageButtonsEl = document.querySelector("#language-buttons");
 
 function formSubmitHandler(event) {
     event.preventDefault();
@@ -83,4 +84,28 @@ function displayRepos(repos, searchTerm) {
     }
 }
 
+function getFeaturedRepos(language){
+    var apiUrl = "https://api.github.com/search/repositories?q=" + language + "+is:featured&sort=help-wanted-issues";
+
+    fetch(apiUrl).then(function(response){
+        if (response.ok){
+            response.json().then(function(data){
+                displayRepos(data.items, language);
+            });
+        }
+        else {
+            alert("Error: " + response.statusText);
+        }
+    });
+}
+
+function buttonClickHanler(event) {
+    var language = event.target.getAttribute("data-language");
+    if(language) {
+        getFeaturedRepos(language);
+        repoContainerEl.textContent = "";
+    }
+}
+
 userFormEl.addEventListener("submit", formSubmitHandler);
+languageButtonsEl.addEventListener("click", buttonClickHanler);
